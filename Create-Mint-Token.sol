b940319f487784837ecc104dc1f2fe10328fe7be
Harsh Thakur <155345451+D1E2V3I4L@users.noreply.github.com>
2024-05-31 18:30:26 +0530

    Update Create-Mint-Token.sol

--- Create-Mint-Token.sol
+++ Create-Mint-Token.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "MTK") {
+        // Mint an initial supply of 1,000,000 tokens to the deployer
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    // Function for the owner to mint new tokens
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Function for any user to burn their tokens
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
 
